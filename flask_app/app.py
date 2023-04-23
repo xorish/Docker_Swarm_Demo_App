8310a66e7ceb065726c5f86ec50983792f5cdbea
sourish <sourishonax@gmail.com>
2023-04-23 19:59:43 +0530

    exp

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import sys
+import os
 
-app = Flask(__name__)
+path = os.path.dirname(os.path.abspath(sys.argv[0]))
+app = Flask(__name__,template_folder=f'{path}/template',static_folder=f"{path}/static")
 
 # Route for the default page
 @app.route("/")
 def home():
-    # Display message
-    return "https://www.google.com"
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=4000)
+    app.run('0.0.0.0',port=8000)
